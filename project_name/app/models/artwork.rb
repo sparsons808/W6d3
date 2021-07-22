d9d369a0aa41ba2935740225571fe89efbc1feb9
Sydney <sydneyparsonsyb@gmail.com>
2021-07-22 12:02:39 -0400

    sharring artwork

---
+++ project_name/app/models/artwork.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  image_url  :string           not null
+#  artist_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Artwork < ApplicationRecord
+    validates :title, uniqueness: { scope: :artist_id }
+
+    belongs_to :artist,
+    primary_key: :id,
+    foreign_key: :artist_id,
+    class_name: :User
+
+    has_many :shared_works,
+    primary_key: :id,
+    foreign_key: :artwork_id,
+    class_name: :ArtworkShare
+
+    has_many :shared_viewers,
+    through: :shared_works,
+    source: :viewer
+end
